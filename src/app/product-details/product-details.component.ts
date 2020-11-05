825b72df2266f142c8171aac9a5fe9219113e226
ampgn <aboucal9@gmail.com>
2020-11-05 12:43:12 +0100

    1 file modified

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -9,6 +9,7 @@ import { products } from "../products";
   styleUrls: ["./product-details.component.css"]
 })
 export class ProductDetailsComponent implements OnInit {
+  product;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
